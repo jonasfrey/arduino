fefcf8a1c091a6c5ce4b4186043f3b296c14c97d
jf18j492 <jonas.frey@csh.unibe.ch>
2021-11-05 17:47:23 +0100

    stepper

--- stepper_star_tracker.ino
+++ stepper_star_tracker.ino
@@ -5,12 +5,6 @@
  
 #define NUMBER_OF_STEPS_PER_REV_FUllSTEPPING 2048
 #define NUMBER_OF_STEPS_PER_REV_HALFSTEPPING 4096
-
-#define DELAY_MICROSECONDS_BETWEEN_SUBSTEP 2
-
-void setup(){
-
-
   // b = arctan(a/b)
   // b = arctan(420/1)
   //   
@@ -23,28 +17,31 @@ void setup(){
   // +-----+
   //  B=1 
   
-  int distance_hinge_to_camera_millimeter = 420
-  int distance_screw_traveling_in_one_revolution_millimeter = 1
+  int distance_hinge_to_camera_millimeter = 234;
+  int distance_screw_traveling_in_one_revolution_millimeter = 1;
   
-  int a = distance_hinge_to_camera_millimeter
-  int b = distance_screw_traveling_in_one_revolution_millimeter
+  int a = distance_hinge_to_camera_millimeter;
+  int b = distance_screw_traveling_in_one_revolution_millimeter;
   
-  double radians_hinge_per_screw_revolution = atan(a/b)
-  double degrees_hinge_per_screw_revolution = (radians_hinge_per_screw_revolution / 6.28318)*360
+  double radians_hinge_per_screw_revolution = atan(a/b);
+  double degrees_hinge_per_screw_revolution = (radians_hinge_per_screw_revolution / 6.28318)*360;
 
   // sidereal period 23 hours, 56 minutes and 4.09053 seconds
-  double degrees_earth_rotation_per_minute = 360/(23*60+56+(4.09/60.0))
+  double degrees_earth_rotation_per_minute = 360/(23*60+56+(4.09/60.0));
 
-  double revolutions_per_minute_to_match_sidereal_period = degrees_earth_rotation_per_minute / degrees_hinge_per_screw_revolution
+  double revolutions_per_minute_to_match_sidereal_period = degrees_earth_rotation_per_minute / degrees_hinge_per_screw_revolution;
 
-  double steps_per_minute_to_match_sidereal_period = revolutions_per_minute_to_match_sidereal_period * NUMBER_OF_STEPS_PER_REV_HALFSTEPPING
+  double steps_per_minute_to_match_sidereal_period = revolutions_per_minute_to_match_sidereal_period * NUMBER_OF_STEPS_PER_REV_HALFSTEPPING;
 
-  double microseconds_per_step = (60*1000*1000) / steps_per_minute_to_match_sidereal_period
+  double microseconds_per_step = (60*1000*1000) / steps_per_minute_to_match_sidereal_period;
 
-  int substeps_per_step = 8 
+  int substeps_per_step = 8;
   
-  double microseconds_per_substep = microseconds_per_step / substeps_per_step
+  double microseconds_per_substep = microseconds_per_step / substeps_per_step;
   
+void setup(){
+
+
 
   pinMode(A,OUTPUT);
   pinMode(B,OUTPUT);
@@ -83,7 +80,7 @@ void loop(){
 
     int i;
     i=0;
-    while(i<NUMBER_OF_STEPS_PER_REV){
+    while(i<NUMBER_OF_STEPS_PER_REV_HALFSTEPPING){
       onestep();
       i++;
     }
